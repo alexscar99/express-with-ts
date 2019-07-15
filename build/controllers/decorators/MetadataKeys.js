581e09d997842c270a100fe692f1f824836bd913
Alex Scarlett <alex.scarlett91@gmail.com>
2019-07-15 16:36:37 -0400

    'use' decorator created for middleware and 'bodyValidator' decorator created to validate the body of the request for the correct properties. Hooked both up as metadata that can be retrieved. Added to enum of MetadataKeys'

--- build/controllers/decorators/MetadataKeys.js
+++ build/controllers/decorators/MetadataKeys.js
@@ -4,4 +4,6 @@ var MetadataKeys;
 (function (MetadataKeys) {
     MetadataKeys["Method"] = "method";
     MetadataKeys["Path"] = "path";
+    MetadataKeys["Middleware"] = "middleware";
+    MetadataKeys["Validator"] = "validator";
 })(MetadataKeys = exports.MetadataKeys || (exports.MetadataKeys = {}));
